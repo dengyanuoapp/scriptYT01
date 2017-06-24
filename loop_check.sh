11f3aaa355e788c9dd35d9896a6417d2c6c65aad
Your Name <you@example.com>
2017-06-24 04:15:38 -0800

    check

--- loop_check.sh
+++ loop_check.sh
@@ -9,6 +9,7 @@ kw4=（
 kw5=《
 kw6=》
 kw7=）
+kw8=：
 
 
 while [ 1 ]
@@ -16,7 +17,7 @@ do
 	#echo 	"$(cat nohup.out |tail -n 1)"
     #   Duration: 00:47:52.33, start: 0.000000, bitrate: 126 kb/s
 	echo -n	" $(cat nohup.out |grep bitrate=|tail -n 1|sed -e 's;[\r\n\t]; ;g' -e 's;^.*size=;size=;g' -e 's; \+; ;g')"
-    echo -n	" $(cat nohup.out |grep Duration|tail -n 1|sed -e 's;^.*Duration: ;;g' -e 's;\..*$;;g'  -e 's; \+; ;g')"
+    echo -n	" $(cat nohup.out |grep Duration|tail -n 1|sed -e 's;^.*Duration: ;;g' -e 's;\..*$;;g'  -e 's; \+; ;g') "
     echo -n	`cat nohup.out |grep Input\ |tail -n 1|sed \
         -e "s;${kw1};_;g"   \
         -e "s;${kw2};_;g"   \
@@ -25,6 +26,7 @@ do
         -e "s;${kw5};_;g"   \
         -e "s;${kw6};_;g"   \
         -e "s;${kw7};_;g"   \
+        -e "s;${kw8};_;g"   \
         -e "s;^[^']*';;g" -e 's;^[0-9_]*;;g' -e "s;'[^']*$;;g" -e "s;[ _\.\-];;g" `
 	echo
 
@@ -39,7 +41,9 @@ do
             re01=$((${now01} - ${last01}))
             re02=$((${need01} - ${re01}))
             echo "${now01}:${line01}:${last01}:${re01}:${re02}"
-        )
-    grep -q 1END1\  nohup.out && echo &&echo&&grep END1 nohup.out && date -d '+16 hour'    +%Y%m%d__%H%M%p && echo && exit 3
+        ) || (
+        tail -n 15 nohup.out | grep -i '\[download\] Downloa' |tail -n 1
+    )
+    tail -n 5 nohup.out | grep -q END1  && echo &&echo&&tail -n 5 nohup.out |grep END1 && date -d '+16 hour'    +%Y%m%d__%H%M%p && echo && exit 3
 	sleep 5
 done
