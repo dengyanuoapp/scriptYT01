268949a439ae340291aab22e0bc007ac88e585ee
yt01 <yt01@jjj123.com>
2017-08-23 15:36:15 +0800

    add du -sh

--- Git_loop_pull.sh
+++ Git_loop_pull.sh
@@ -4,11 +4,13 @@ while [ 1 = 1 ]
 do
     echo ; echo 'befor pull ' ; echo
     df -h .
+    du -sh .
 
     git pull
 
     echo ; echo 'end pull , sleep 30 minutes ' ; echo
     df -h .
+    du -sh .
 
     rm -f `find -name "*.amr"`
     pwd
