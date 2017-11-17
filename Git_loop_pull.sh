1d3b337e329503f258fadf4aa6cef3471e4577a3
Your Name <you@example.com>
2017-11-17 22:16:34 +0800

    add the vpn2

--- Git_loop_pull.sh
+++ Git_loop_pull.sh
@@ -10,8 +10,16 @@ do
     then
         git pull
     else
-        echo "https_proxy=${1}:22224"
-              https_proxy=${1}:22224 git pull
+        if echo ${1}|grep -q ':'
+        then
+            echo "https_proxy=${1}"
+                  https_proxy=${1}       git pull
+            echo "https_proxy=${1}"
+        else
+            echo "https_proxy=${1}:22224"
+                  https_proxy=${1}:22224 git pull
+            echo "https_proxy=${1}:22224"
+        fi
     fi
 
     echo ; echo 'end pull , sleep 30 minutes ' ; echo
