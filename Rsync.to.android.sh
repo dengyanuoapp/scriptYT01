899f76abcb7841e9c9b7c4db3ecf4fb768e6a538
Your Name <you@example.com>
2017-12-02 05:15:27 -0800

    aaa

--- Rsync.to.android.sh
+++ Rsync.to.android.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 aaa1=192.168.197.49
+aaa2=/sdcard/9/
 [ -z "$1" ] || aaa1=$1
 
 echo 
-echo " to .... ${aaa1}"
+echo " to .... ${aaa1} ${aaa2} "
 
 nice -n 19 \
     rsync -r -v -l -c \
     --delete --inplace --omit-dir-times --no-perms --size-only \
     -e "ssh -p 22225"  \
     ./  \
-    aa@${aaa1}:/sdcard/9/
+    aa@${aaa1}:{aaa2}
